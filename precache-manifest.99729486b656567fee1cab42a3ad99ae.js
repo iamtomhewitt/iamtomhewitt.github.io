3a9663ad9a205581082d6a14f12af9f95da4dd28
Tom Hewitt <Tom.Hewitt@williamhill.com>
2019-08-29 14:35:09 +0100

    Updates

---
+++ precache-manifest.99729486b656567fee1cab42a3ad99ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae2474f5c40e4f1a9b3242d3decbe7c",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "9b0ab1745143f252ee60",
+    "url": "/website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ca726100630d08f3ed2b",
+    "url": "/website/static/js/2.b56e66db.chunk.js"
+  },
+  {
+    "revision": "9b0ab1745143f252ee60",
+    "url": "/website/static/js/main.f23d1839.chunk.js"
+  },
+  {
+    "revision": "dc06eda8a43fceffdcf1",
+    "url": "/website/static/js/runtime~main.357ec5e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
