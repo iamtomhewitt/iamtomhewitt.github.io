379deb2a749f41d44948ae43dd89184508ceb4f0
Tom Hewitt <tommhewitt@gmail.com>
2019-08-30 15:17:53 +0100

    Add some simple routing to Projects, and anything else to PageNotFound

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PageNotFound = () => <h1>Page not found, maybe I've not built that yet!</h1>
+
+export default PageNotFound
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import Projects from './components/Projects';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import PageNotFound from './components/PageNotFound';
+
+const routing = (
+    <Router>
+      <div>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route exact path="/projects" component={Projects} />
+            <Route component={PageNotFound}/>
+        </Switch>
+      </div>
+    </Router>
+  )
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
