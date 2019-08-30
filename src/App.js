ae46d452bb6842a8b489bf173fe777e8f82868ad
Tom Hewitt <tommhewitt@gmail.com>
2019-08-30 14:57:23 +0100

    Add basic home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import Home from './components/Home'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home></Home>
+      </div>
+    );
+  }
 }
 
 export default App;
