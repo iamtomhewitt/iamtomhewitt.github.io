3483d76489cc981805eb1ec667579fa3ac6bbc0a
Tom Hewitt <tommhewitt@gmail.com>
2019-09-09 18:05:37 +0100

    Setup simple Projects and Experience pages, as well as a PageNotFound page
    Also added
    - react-router-bootstrap
    - More text to Home
    - Projects and Experience routing to Routes
    - Changed text colour on NavBar on links

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import PageNotFound from "./containers/PageNotFound";
+import Experience from "./containers/Experience";
+import Projects from "./containers/Projects";
+
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/experience" exact component={Experience} />
+    <Route path="/projects" exact component={Projects} />
+
+    { /* Catch all unmatched routes */}
+    <Route component={PageNotFound} />
   </Switch>;
---
+++ src/containers/Experience.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Experience.css";
+
+export default class Experience extends Component {
+    render() {
+        return (
+            <div className="Experience">
+                <h1>Experience</h1>
+            </div>
+        );
+    }
+}
