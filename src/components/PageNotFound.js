7694ecc1d3e01b962adb94484b75dd003b0372b7
Tom Hewitt <tommhewitt@gmail.com>
2019-09-09 17:45:36 +0100

    Strip back project, add favicons, react router and react bootstrap

--- src/components/PageNotFound.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const PageNotFound = () => <h1>Page not found, maybe I've not built that yet!</h1>
-
-export default PageNotFound
--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
-import { Route, HashRouter as Router, Switch } from 'react-router-dom'
-import Projects from './components/Projects';
-import PageNotFound from './components/PageNotFound';
+
+import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
-import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
-const routing = (
+ReactDOM.render(
     <Router>
-      <div>
-          <Switch>
-            <Route exact path="/" component={App} />
-            <Route exact path="/projects" component={Projects} />
-            <Route component={PageNotFound}/>
-        </Switch>
-      </div>
-    </Router>
-  )
-
-ReactDOM.render(routing, document.getElementById('root'));
-
+      <App />
+    </Router>,
+    document.getElementById("root")
+  );
+  
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
