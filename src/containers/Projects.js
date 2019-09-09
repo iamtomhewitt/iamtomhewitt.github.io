a059eeeb426167b68825dbfb00dc39e4d45b681e
Tom Hewitt <tommhewitt@gmail.com>
2019-09-09 19:46:12 +0100

    Started GitHub api integration

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -2,8 +2,52 @@ import React, { Component } from "react";
 import "./Projects.css";
 
 export default class Projects extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            repos: null
+        };
+    }
+    componentDidMount() {
+        var repos = []
+        var request = new XMLHttpRequest()
+
+        request.open('GET', 'https://api.github.com/users/iamtomhewitt/repos', true)
+        request.onload = function () {
+            var data = JSON.parse(this.response)
+
+            var name = '';
+            var repo_url = '';
+            var repo_description = '';
+
+            for (const [key, value] of Object.entries(data)) {
+                name = value['name']
+                repo_url = value['html_url']
+                repo_description = value['description']
+
+                repos.push(
+                    {
+                        repo_name: name,
+                        url: repo_url,
+                        description: repo_description
+                    }
+                )
+            }
+        }
+
+        this.setState({
+            repos: repos
+        });
+
+        request.send()
+    }
+
     render() {
+        console.log(this.state.repos);
+
         return (
+            
             <div className="Projects">
                 <h1>Projects</h1>
                 <p>This is where a table of my projects live. This could be manual, or an API request to my github listing all of my projects in a table.</p>
