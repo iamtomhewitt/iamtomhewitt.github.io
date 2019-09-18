6f4c3aa057a81e2004775c6fc41762c05a62c186
Tom Hewitt <tommhewitt@gmail.com>
2019-09-18 17:22:57 +0100

    Add a timeline for experience

--- src/containers/Experience.js
+++ src/containers/Experience.js
@@ -1,31 +1,46 @@
 import React, { Component } from "react";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+
+import 'react-vertical-timeline-component/style.min.css';
 import "./Experience.css";
 
 export default class Experience extends Component {
     render() {
+        var s = {
+            "background-color": "#F2F3F4"
+        };
         return (
-            <div className="Experience">
-                <h1>Experience</h1>
-                <br></br>
-                {/* LinkedAPI no longer available, add manually for now */}
-                <table id="experience-table">
-                    <thead>
-                        <tr>
-                            <td><b>Title</b></td>
-                            <td><b>Where</b></td>
-                            <td><b>Date</b></td>
-                            <td><b>Doing</b></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Developer</td>
-                            <td>William Hill</td>
-                            <td>July 2019</td>
-                            <td>Java</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div>
+                <div>
+                    <h1>Where I've worked</h1>
+                </div>
+                <div style={s}>
+                    <VerticalTimeline >
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2019 - present"
+                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        >
+                            <h3 className="vertical-timeline-element-title">Developer</h3>
+                            <h4 className="vertical-timeline-element-subtitle">William Hill</h4>
+                            <p>
+                                Doing some coding
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2018 - 2019"
+                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        >
+                            <h3 className="vertical-timeline-element-title">Developer</h3>
+                            <h4 className="vertical-timeline-element-subtitle">CGI</h4>
+                            <p>
+                                Doing some coding
+                            </p>
+                        </VerticalTimelineElement>
+
+                    </VerticalTimeline>
+                </div>
             </div>
         );
     }
