f7cb506c3aa4518506c285379411748c11c3d1e7
Tom Hewitt <tommhewitt@gmail.com>
2019-09-18 16:00:11 +0100

    Simple experience table as LinkedInAPI no longer available

--- src/containers/Experience.js
+++ src/containers/Experience.js
@@ -6,7 +6,26 @@ export default class Experience extends Component {
         return (
             <div className="Experience">
                 <h1>Experience</h1>
-                <p>List of my experience like LinkedIn. Perhaps there is a LinkedIn api that I could use?</p>
+                <br></br>
+                {/* LinkedAPI no longer available, add manually for now */}
+                <table id="experience-table">
+                    <thead>
+                        <tr>
+                            <td><b>Title</b></td>
+                            <td><b>Where</b></td>
+                            <td><b>Date</b></td>
+                            <td><b>Doing</b></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Developer</td>
+                            <td>William Hill</td>
+                            <td>July 2019</td>
+                            <td>Java</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
