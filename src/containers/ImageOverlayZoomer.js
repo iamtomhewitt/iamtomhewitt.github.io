de1882c3db4ead8446fc7447b075de5436d0101a
Tom Hewitt <tommhewitt@gmail.com>
2019-09-19 13:24:29 +0100

    Add an example for Github hover overlay tiles with links

---
+++ src/containers/ImageOverlayZoomer.js
@@ -0,0 +1,70 @@
+import React, { Component} from "react";
+import image from '../images/image.jpg';
+import "./ImageOverlayZoomer.css";
+
+export default class ImageOverlayZoomer extends Component {
+
+    render() {
+
+        return (
+            <div className="ImageOverlayZoomer">
+                <h1 className="title">Projects</h1>
+
+                <div className="centered">
+                    <div className="content">
+                        <a href="www.google.com" target="_blank">
+                            <div className="content-overlay"></div>
+                            <img className="content-image" alt="" src={image} />
+                            <div className="content-details fadeIn-bottom">
+                                <h3 className="content-title">This is a title</h3>
+                                <p className="content-text">This is a sddddddhort description</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="content">
+                        <a href="www.google.com" target="_blank">
+                            <div className="content-overlay"></div>
+                            <img className="content-image" alt="" src={image} />
+                            <div className="content-details fadeIn-bottom">
+                                <h3 className="content-title">This is a title</h3>
+                                <p className="content-text">This is a short description</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="content">
+                        <a href="www.google.com" target="_blank">
+                            <div className="content-overlay"></div>
+                            <img className="content-image" alt="" src={image} />
+                            <div className="content-details fadeIn-bottom">
+                                <h3 className="content-title">This is a title</h3>
+                                <p className="content-text">This is a short description</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className="centered">
+                    <div className="content">
+                        <a href="www.google.com" target="_blank">
+                            <div className="content-overlay"></div>
+                            <img className="content-image" alt="" src={image} />
+                            <div className="content-details fadeIn-bottom">
+                                <h3 className="content-title">This is a title</h3>
+                                <p className="content-text">This is a sddddddhort description</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="content">
+                        <a href="www.google.com" target="_blank">
+                            <div className="content-overlay"></div>
+                            <img className="content-image" alt="" src={image} />
+                            <div className="content-details fadeIn-bottom">
+                                <h3 className="content-title">This is a title</h3>
+                                <p className="content-text">This is a short description</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
