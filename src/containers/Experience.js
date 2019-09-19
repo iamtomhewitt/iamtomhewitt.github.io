497e04963fdbec287fb442d998583f4d85c02f5e
Tom Hewitt <tommhewitt@gmail.com>
2019-09-19 08:55:38 +0100

    Experience page css updates

--- src/containers/Experience.js
+++ src/containers/Experience.js
@@ -10,26 +10,27 @@ export default class Experience extends Component {
             "background-color": "#F2F3F4"
         };
         return (
-            <div>
+            <div class="Experience">
                 <div>
-                    <h1>Where I've worked</h1>
+                    <h1 className="vertical-timeline-element-title">Where I've worked</h1>
+                    <br></br>
                 </div>
                 <div style={s}>
                     <VerticalTimeline >
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
-                            date="2019 - present"
+                            date="July 2019 - present"
                             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                         >
                             <h3 className="vertical-timeline-element-title">Developer</h3>
                             <h4 className="vertical-timeline-element-subtitle">William Hill</h4>
                             <p>
-                                Doing some coding
+                                Doing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some codingDoing some coding
                             </p>
                         </VerticalTimelineElement>
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
-                            date="2018 - 2019"
+                            date="July 2017 - July 2019"
                             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                         >
                             <h3 className="vertical-timeline-element-title">Developer</h3>
