47abd6913ad7641101a412f841bc0527e9f93b02
Tom Hewitt <tommhewitt@gmail.com>
2019-09-20 15:01:42 +0100

    Get grid working on projects page

--- src/containers/GithubTile.js
+++ src/containers/GithubTile.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import image from '../images/image.jpg';
 import "./GithubTile.css";
 
@@ -16,70 +16,3 @@ export function Tile(repos) {
         </div>
     );
 }
-
-// export default class ImageOverlayZoomer extends Component {
-
-//     render() {
-
-//         return (
-//             <div className="ImageOverlayZoomer">
-//                 <h1 className="title">Projects</h1>
-
-//                 <div className="centered">
-//                     <div className="content">
-//                         <a href="www.google.com" target="_blank">
-//                             <div className="content-overlay"></div>
-//                             <img className="content-image" alt="" src={image} />
-//                             <div className="content-details fadeIn-bottom">
-//                                 <h3 className="content-title">This is a title</h3>
-//                                 <p className="content-text">This is a sddddddhort description</p>
-//                             </div>
-//                         </a>
-//                     </div>
-//                     <div className="content">
-//                         <a href="www.google.com" target="_blank">
-//                             <div className="content-overlay"></div>
-//                             <img className="content-image" alt="" src={image} />
-//                             <div className="content-details fadeIn-bottom">
-//                                 <h3 className="content-title">This is a title</h3>
-//                                 <p className="content-text">This is a short description</p>
-//                             </div>
-//                         </a>
-//                     </div>
-//                     <div className="content">
-//                         <a href="www.google.com" target="_blank">
-//                             <div className="content-overlay"></div>
-//                             <img className="content-image" alt="" src={image} />
-//                             <div className="content-details fadeIn-bottom">
-//                                 <h3 className="content-title">This is a title</h3>
-//                                 <p className="content-text">This is a short description</p>
-//                             </div>
-//                         </a>
-//                     </div>
-//                 </div>
-//                 <div className="centered">
-//                     <div className="content">
-//                         <a href="www.google.com" target="_blank">
-//                             <div className="content-overlay"></div>
-//                             <img className="content-image" alt="" src={image} />
-//                             <div className="content-details fadeIn-bottom">
-//                                 <h3 className="content-title">This is a title</h3>
-//                                 <p className="content-text">This is a sddddddhort description</p>
-//                             </div>
-//                         </a>
-//                     </div>
-//                     <div className="content">
-//                         <a href="www.google.com" target="_blank">
-//                             <div className="content-overlay"></div>
-//                             <img className="content-image" alt="" src={image} />
-//                             <div className="content-details fadeIn-bottom">
-//                                 <h3 className="content-title">This is a title</h3>
-//                                 <p className="content-text">This is a short description</p>
-//                             </div>
-//                         </a>
-//                     </div>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
