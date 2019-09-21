8af8dec58319c83204239a5da1d1adcf892232e9
Tom Hewitt <tommhewitt@gmail.com>
2019-09-21 18:39:49 +0100

    Add profile picture to homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,9 +7,13 @@ export default class Home extends Component {
             <div className="Home">
                 <div className="lander">
                     <h1>Hi, I'm Tom!</h1>
-                    <p>I'm currently a software developer at CGI, working in the defence sector.</p>
-                    <p>Nottingham Trent University graduate with a First Class Honours in Computer Science (Games Technology). My final year project involved creating a virtual reality interactive model of the human heart to assess its usefulness as an alternative learning style for medical students (as compared to 2D diagrams and textbooks).</p>
-                    <p>​I play drums, games, and like to learn about new software technologies. And love a bit of metal.</p>
+                    <div className="profile">
+                    <img src={process.env.PUBLIC_URL + '/images/Me.jpg'} />
+                    </div>
+                    <p/>
+                    <p>I'm currently a software developer at William Hill. I play drums, a few video games (as well as make my own on Google Play!). And love a bit of metal.</p>
+                    <p>I graduated from Nottingham Trent University with a First Class Honours in Computer Science (Games Technology)</p>
+                    <p>My final year project involved creating a virtual reality interactive model of the human heart to assess its usefulness as an alternative learning style for medical students (as compared to 2D diagrams and textbooks).</p>
                 </div>
             </div>
         );
