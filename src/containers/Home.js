8b64d28d80c45d4f394d46cb5a1b192b9fd24344
Tom Hewitt <tommhewitt@gmail.com>
2019-09-22 16:41:19 +0100

    Add icons to experience

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,9 +8,9 @@ export default class Home extends Component {
                 <div className="lander">
                     <h1>Hi, I'm Tom!</h1>
                     <div className="profile">
-                    <img src={process.env.PUBLIC_URL + '/images/Me.jpg'} />
+                        <img src={process.env.PUBLIC_URL + '/images/Me.jpg'} />
                     </div>
-                    <p/>
+                    <p />
                     <p>I'm currently a software developer at William Hill. I play drums, a few video games (as well as make my own on Google Play!). And love a bit of metal.</p>
                     <p>I graduated from Nottingham Trent University with a First Class Honours in Computer Science (Games Technology)</p>
                     <p>My final year project involved creating a virtual reality interactive model of the human heart to assess its usefulness as an alternative learning style for medical students (as compared to 2D diagrams and textbooks).</p>
