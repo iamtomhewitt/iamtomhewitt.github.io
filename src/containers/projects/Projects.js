88260937d50cdde3dc3e9bdd9a69f512003686f8
Tom Hewitt <tommhewitt@gmail.com>
2019-12-03 13:57:32 +0000

    Make the tile image the github.png image that appears in the repo

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -5,72 +5,73 @@ import "./Projects.css";
 
 export default class Projects extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            repos: []
-        };
-    }
+	constructor() {
+		super();
+		this.state = {
+			repos: []
+		};
+	}
 
-    componentDidMount() {
-        const that = this;
-        fetch("https://api.github.com/users/iamtomhewitt/repos")
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (jsonData) {
-                var fetched_repos = []
-                var data = jsonData
+	componentDidMount() {
+		const that = this;
+		fetch("https://api.github.com/users/iamtomhewitt/repos")
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (jsonData) {
+				var fetched_repos = []
+				var data = jsonData
 
-                for (var i = 0; i < data.length; i++) {
-                    var repo = data[i];
-                    fetched_repos.push(
-                        {
-                            name: repo.name,
-                            url: repo.html_url,
-                            description: repo.description,
-                            language: repo.language,
-                            updatedAt: repo.updated_at
-                        }
-                    )
-                }
-                fetched_repos.sort(function (a, b) {
-                    var dateA = new Date(a.updatedAt), dateB = new Date(b.updatedAt);
-                    return dateB - dateA;
-                });
-                that.setState({ repos: fetched_repos });
-            });
-    }
+				for (var i = 0; i < data.length; i++) {
+					var repo = data[i];
+					fetched_repos.push(
+						{
+							name: repo.name,
+							url: repo.html_url,
+							description: repo.description,
+							language: repo.language,
+							updatedAt: repo.updated_at,
+							imageSource: "https://raw.githubusercontent.com/iamtomhewitt/"+repo.name+"/master/github.png"
+						}
+					)
+				}
+				fetched_repos.sort(function (a, b) {
+					var dateA = new Date(a.updatedAt), dateB = new Date(b.updatedAt);
+					return dateB - dateA;
+				});
+				that.setState({ repos: fetched_repos });
+			});
+	}
 
-    createGrid = () => {
-        let grid = []
-        let githubTiles = []
-        let rows = []
-        let itemsPerRow = 4 
+	createGrid = () => {
+		let grid = []
+		let githubTiles = []
+		let rows = []
+		let itemsPerRow = 4 
 
-        // First create tiles out of all the repos retrieved
-        for (let i = 0; i < this.state.repos.length; i++) {
-            githubTiles.push(<Tile key={i} {...this.state.repos[i]} />);
-        }
+		// First create tiles out of all the repos retrieved
+		for (let i = 0; i < this.state.repos.length; i++) {
+			githubTiles.push(<Tile key={i} {...this.state.repos[i]} />);
+		}
 
-        // Split them into groups
-        while (githubTiles.length > 0) {
-            rows.push(githubTiles.splice(0, itemsPerRow));
-        }
+		// Split them into groups
+		while (githubTiles.length > 0) {
+			rows.push(githubTiles.splice(0, itemsPerRow));
+		}
 
-        // Now create a div for the groups of three to sit in
-        for (let i = 0; i < rows.length; i++) {
-            grid.push(<div key={i} className="centered">{rows[i]}</div>)
-        }
+		// Now create a div for the groups to sit in
+		for (let i = 0; i < rows.length; i++) {
+			grid.push(<div key={i} className="centered">{rows[i]}</div>)
+		}
 
-        return grid;
-    }
+		return grid;
+	}
 
-    render() {
-        if (this.state.repos.length > 0) {
-            return (
-                <div className="Projects">
-                    <h1>I've built</h1>
+	render() {
+		if (this.state.repos.length > 0) {
+			return (
+				<div className="Projects">
+					<h1>I've built</h1>
                     {this.createGrid()}
                 </div>
             );
@@ -79,9 +80,9 @@ export default class Projects extends Component {
             return (
                 <div className="Projects">
                     <h1>I've built</h1>
-                    <p>No repos found :-(</p>
-                </div>
-            );
-        }
-    }
+					<p>No repos found :-(</p>
+				</div>
+			);
+		}
+	}
 }
