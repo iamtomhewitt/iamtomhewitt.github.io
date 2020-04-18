5173d6492128820091d7e0c1dbe44ed8279d5ed6
Tom Hewitt <tommhewitt@gmail.com>
2020-04-18 20:49:29 +0100

    🎨 style(experience): Add title

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -6,40 +6,43 @@ import "./Experience.css";
 export default class Experience extends Component {
 	render() {
 		return (
-			<div id="container">
-				<Tile
-					jobTitle="Developer"
-					company="William Hill"
-					description="Working in the Retail Tech department, on a Retail Operational Excellence project. Currently working on betting terminal fixes and in house dashboards for other projects."
-					time="Jul 2019 - Present"
-					fontSize="16"
-				/>
+			<>
+				<h1 id="title">I've worked at</h1>
+				<div id="container">
+					<Tile
+						jobTitle="Developer"
+						company="William Hill"
+						description="Working in the Retail Tech department, on a Retail Operational Excellence project. Currently working on betting terminal fixes and in house dashboards for other projects."
+						time="Jul 2019 - Present"
+						fontSize="16"
+					/>
 
-				<Tile
-					jobTitle="Graduate Developer"
-					company="CGI"
-					description="Responsible for an entire customer relationship type subsystem, using a variety of tools and software such as Java, SQL, JSP, Perl, Hibernate, VB, Remedy, Crystal Reports. Subsystem compromises of a workflow tool, an in-house website and a reporting tool."
-					time="Aug 2017 - Jul 2019"
-					fontSize="14"
-				/>
+					<Tile
+						jobTitle="Graduate Developer"
+						company="CGI"
+						description="Responsible for an entire customer relationship type subsystem, using a variety of tools and software such as Java, SQL, JSP, Perl, Hibernate, VB, Remedy, Crystal Reports. Subsystem compromises of a workflow tool, an in-house website and a reporting tool."
+						time="Aug 2017 - Jul 2019"
+						fontSize="14"
+					/>
 
-				<Tile
-					jobTitle="Analyst Programmer"
-					company="CGI"
-					description="Working in the Defence Sector at CGI, as part of an industrial placement year for university."
-					time="Aug 2015 - Jul 2016"
-					fontSize="16"
-				/>
+					<Tile
+						jobTitle="Analyst Programmer"
+						company="CGI"
+						description="Working in the Defence Sector at CGI, as part of an industrial placement year for university."
+						time="Aug 2015 - Jul 2016"
+						fontSize="16"
+					/>
 
-				<Tile
-					jobTitle="Student Ambassador"
-					company="Nottingham Trent University"
-					description="Personally invited onto the Student Ambassador scheme due to high achievements from first year.
+					<Tile
+						jobTitle="Student Ambassador"
+						company="Nottingham Trent University"
+						description="Personally invited onto the Student Ambassador scheme due to high achievements from first year.
 					Supported first year students in programming sessions and one on one mentoring sessions, as well as helping out and leading groups of potential students at university open days."
-					time="Sep 2014 - Jun 2015"
-					fontSize="14"
-				/>
-			</div>
+						time="Sep 2014 - Jun 2015"
+						fontSize="14"
+					/>
+				</div>
+			</>
 		);
 	}
 }
