50e3c2af9c4b48018f4d163578e8745b23a5d934
Tom Hewitt <tommhewitt@gmail.com>
2020-04-18 20:54:33 +0100

    🎨 style(page-not-found): Update css

--- src/components/page-not-found/PageNotFound.js
+++ src/components/page-not-found/PageNotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./PageNotFound.css";
 
 export default () =>
-  <div className="PageNotFound">
-    <h3>Page not found!</h3>
-    <p>I might not have built this yet!</p>
-  </div>;
+	<div id="pageNotFound">
+		<h3>Page not found!</h3>
+		<p>I might not have built this yet!</p>
+	</div>;
