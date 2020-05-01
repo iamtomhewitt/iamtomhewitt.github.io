b68532149442380e368170eeadd225d695cc564c
Tom Hewitt <tommhewitt@gmail.com>
2020-05-01 23:34:38 +0100

    💻 feat(experience): Add an image to each experience tile

--- src/components/experience/Tile.js
+++ src/components/experience/Tile.js
@@ -9,9 +9,16 @@ export default class Tile extends React.Component {
 		};
 		return (
 			<div id="tile">
-				<div id="jobTitle">{this.props.jobTitle}</div>
-				<div id="company">{this.props.company}</div>
-				<div id="time">{this.props.time}</div>
+				<div id="quickInfo">
+					<div id="details">
+						<div id="jobTitle">{this.props.jobTitle}</div>
+						<div id="company">{this.props.company}</div>
+						<div id="time">{this.props.time}</div>
+					</div>
+					<div id="image">
+						<img src={this.props.logo}></img>
+					</div>
+				</div>
 				<div id="description" style={fontSize}>{this.props.description}</div>
 			</div>
 		)
