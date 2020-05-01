45bf6900cd2bf59ac5b035ea68f57ef5337ebcfc
Tom Hewitt <tommhewitt@gmail.com>
2020-05-01 23:35:03 +0100

    🎨 style(experience): Add the images to each tile

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Tile from "./Tile";
+import wh from '../../images/william-hill.png';
+import cgi from '../../images/cgi.png';
+import ntu from '../../images/ntu.png';
 
 import "./Experience.css";
 
@@ -12,8 +15,9 @@ export default class Experience extends Component {
 					<Tile
 						jobTitle="Developer"
 						company="William Hill"
-						description="Working in Retail, currently working on the software for self service betting terminals."
+						description="Working in Retail, currently working on the software for self service betting terminals. During the coronavirus outbreak I have been working on William Hill's Bingo website."
 						time="Jul 2019 - Present"
+						logo={wh}
 						fontSize="16"
 					/>
 
@@ -22,6 +26,7 @@ export default class Experience extends Component {
 						company="CGI"
 						description="Responsible for an entire customer relationship type subsystem, using a variety of tools and software such as Java, SQL, JSP, Perl, Hibernate, VB, Remedy, Crystal Reports. Subsystem compromises of a workflow tool, an in-house website and a reporting tool."
 						time="Aug 2017 - Jul 2019"
+						logo={cgi}
 						fontSize="14"
 					/>
 
@@ -30,15 +35,16 @@ export default class Experience extends Component {
 						company="CGI"
 						description="Working in the Defence Sector at CGI, as part of an industrial placement year for university."
 						time="Aug 2015 - Jul 2016"
+						logo={cgi}
 						fontSize="16"
 					/>
 
 					<Tile
 						jobTitle="Student Ambassador"
 						company="Nottingham Trent University"
-						description="Personally invited onto the Student Ambassador scheme due to high achievements from first year.
-					Supported first year students in programming sessions and one on one mentoring sessions, as well as helping out and leading groups of potential students at university open days."
+						description="Personally invited onto the Student Ambassador scheme due to high achievements from first year. Supported first year students in programming sessions and one on one mentoring sessions, as well as helping out and leading groups of potential students at university open days."
 						time="Sep 2014 - Jun 2015"
+						logo={ntu}
 						fontSize="14"
 					/>
 				</div>
